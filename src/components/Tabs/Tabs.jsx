6f9809635a504afe9602d5e63d675901d59f6c2c
Tetiana Chuhai <chugaytatyana@gmail.com>
2025-11-24 21:21:26 +0200

    correction

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,41 +2,37 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) ?? tabs[0];
 
   return (
-    <>
-      <h1 className="title">{`Selected tab is ${activeTab.title}`}</h1>
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const isActive = tab.id === activeTab.id;
 
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            {tabs.map(tab => {
-              const isActive = tab.id === activeTab.id;
-
-              return (
-                <li
-                  key={tab.id}
-                  className={isActive ? 'is-active' : ''}
-                  data-cy="Tab"
+            return (
+              <li
+                key={tab.id}
+                className={isActive ? 'is-active' : ''}
+                data-cy="Tab"
+              >
+                <a
+                  href={`#${tab.id}`}
+                  data-cy="TabLink"
+                  onClick={e => {
+                    e.preventDefault();
+                    if (!isActive) onTabSelected(tab.id);
+                  }}
                 >
-                  <a
-                    href={`#${tab.id}`}
-                    data-cy="TabLink"
-                    onClick={e => {
-                      e.preventDefault();
-                      if (!isActive) onTabSelected(tab.id);
-                    }}
-                  >
-                    {tab.title}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          {activeTab.content}
-        </div>
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab.content}
+      </div>
+    </div>
   );
 };
